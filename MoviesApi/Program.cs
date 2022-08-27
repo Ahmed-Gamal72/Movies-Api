d0ba768d254469c3a0494f6082cd4707c91c792c
Jimmy Neotron <ahmedgad1497@gmail.com>
2022-08-27 23:55:31 +0200

    Add project files.

---
+++ MoviesApi/Program.cs
@@ -0,0 +1,75 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "TestApi",
+        Description = "My first api",
+        TermsOfService = new Uri("https://www.google.com"),
+        Contact = new OpenApiContact
+        {
+            Name = "Ahmed",
+            Email = "test@doman.com",
+            Url = new Uri("https://www.google.com")
+        },
+        License = new OpenApiLicense()
+        {
+            Name="my Licence",
+            Url= new Uri("https://www.google.com")
+        }
+    });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name="Authorization",
+        Type=SecuritySchemeType.ApiKey,
+        Scheme= "Bearer",
+        BearerFormat="JWT",
+        In=ParameterLocation.Header,
+        Description="Enter your JWT key"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference= new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id= "Bearer"
+                },
+                Name ="Bearer",
+                In=ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(c=>c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

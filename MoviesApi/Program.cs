e1f97fdcfa897d3168e8fc075d8cee640d1d4750
Jimmy Neotron <ahmedgad1497@gmail.com>
2022-08-28 17:40:46 +0200

    Add DbContext and connectionString

--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using MoviesApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
